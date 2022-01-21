e73ebad3a65de06e57445e1140b69cfd9597a808
Saepudin <saepudinjeh@gmail.com>
2022-01-21 23:55:23 +0700

    update middlewares

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,5 +1,6 @@
 module.exports = [
   'strapi::errors',
+  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
@@ -7,18 +8,4 @@ module.exports = [
   'strapi::body',
   'strapi::favicon',
   'strapi::public',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  }
 ];
